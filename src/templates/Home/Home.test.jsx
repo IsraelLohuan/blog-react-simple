682f47d47ba046b4e9c41cb4cfeb36c918f518e8
Israel Lohuan <israel@globalsistemas.com.br>
2021-04-26 22:30:38 -0300

    Finalizado testes

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -3,6 +3,7 @@ import { Home } from ".";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 import userEvent from '@testing-library/user-event';
+import { useEffect } from "react";
 
 const handlers = [
   rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
@@ -77,11 +78,11 @@ describe('<Home />', () => {
 
     jest.setTimeout(10000);
 
-    const { debug } = render(<Home />);
+    render(<Home />);
 
     const noMorePosts = screen.queryByText("Não existem posts");
 
-    //expect.assertions(3);
+    expect.assertions(10);
 
     await waitForElementToBeRemoved(noMorePosts);
 
@@ -89,22 +90,50 @@ describe('<Home />', () => {
 
     expect(screen.getByRole("heading", { name: "title1 1" }))
     .toBeInTheDocument();
-
     expect(screen.getByRole("heading", { name: "title2 2" }))
     .toBeInTheDocument();
-
     expect(screen.queryByRole("heading", { name: "title3 3" }))
     .not.toBeInTheDocument();
 
     userEvent.type(search, "title1");
-
     expect(screen.getByRole("heading", { name: "title1 1" }))
     .toBeInTheDocument();
-
     expect(screen.queryByRole("heading", { name: "title2 2" }))
     .not.toBeInTheDocument();
-
     expect(screen.queryByRole("heading", { name: "title3 3" }))
     .not.toBeInTheDocument();
+
+    expect(screen.getByRole("heading", {name: "Search value: title1"})).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole("heading", { name: "title1 1" }))
+    .toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: "title2 2" }))
+    .toBeInTheDocument();
+
+    userEvent.type(search, "post does not exist");
+    expect(screen.getByText("Não existem posts")).toBeInTheDocument();
+  });
+
+  it('should load more posts when button', async () => {
+
+    jest.setTimeout(10000);
+
+    const { debug } = render(<Home />);
+
+    const noMorePosts = screen.queryByText("Não existem posts");
+
+    expect.assertions(2);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByRole("button", { name: /load more posts/i});
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading", { name: "title3 3" }))
+    .toBeInTheDocument();
+
+    expect(button).toBeDisabled();
   });
 });
